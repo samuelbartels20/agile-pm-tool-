022695b72ea78d175fe86c194eee787b84ff016d
samuelbartels20 <bartelssamuel20@gmail.com>
2021-03-27 04:36:01 +0000

    added date

--- src/main/java/com/skillembassy/pmtool/domain/Project.java
+++ src/main/java/com/skillembassy/pmtool/domain/Project.java
@@ -1,8 +1,16 @@
 package com.skillembassy.pmtool.domain;
 
+import java.util.Date;
+
 public class Project {
 
 
     private long id;
-    
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+
+    private Date created_At;
+    private Date update_At;
 }
